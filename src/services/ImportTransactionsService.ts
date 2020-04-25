e82935ce7dc47c3fc00caf3bb9b6c1d1392a64e4
Manogel <manoelgomes53@gmail.com>
2020-04-25 12:20:02 -0300

    added cors and code refactoring

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -16,50 +16,13 @@ interface CreateManyTransactionsDTO {
   category: string;
 }
 
-async function csvJSON(csvPath: string): Promise<CreateManyTransactionsDTO[]> {
-  const csv = await new Promise<Array<string[]>>(resolve => {
-    const listLineCsv: Array<string[]> = [];
-    fs.createReadStream(csvPath)
-      .pipe(csvparse())
-      .on('data', row => {
-        listLineCsv.push(row);
-      })
-      .on('end', () => {
-        resolve(listLineCsv);
-      });
-  });
-
-  const result: CreateManyTransactionsDTO[] = [];
-
-  const headers: string[] = csv[0].map(t => t.trim());
-
-  for (let i = 1; i < csv.length; i++) {
-    const currentline = csv[i];
-
-    /*  for (let j = 0; j < headers.length; j++) {
-      const indexName: string = headers[j].trim();
-      const valueIndex: string = currentline[j].trim();
-      transaction.title = valueIndex;
-    }
- */
-    const transaction: CreateManyTransactionsDTO = Object.assign(
-      {},
-      ...headers.map((k, i) => ({ [k]: currentline[i].trim() })),
-    );
-
-    result.push(transaction);
-  }
-
-  return result;
-}
-
 class ImportTransactionsService {
   async execute(data: Request): Promise<Transaction[]> {
     const { path } = data;
 
     const transactionRepository = getCustomRepository(TransactionsRepository);
 
-    const arrayJsonTransactions: CreateManyTransactionsDTO[] = await csvJSON(
+    const arrayJsonTransactions: CreateManyTransactionsDTO[] = await this.convertCSVtoJSON(
       path,
     );
 
@@ -73,6 +36,39 @@ class ImportTransactionsService {
     }
 
     return transactions;
+  }
+
+  async convertCSVtoJSON(
+    csvPath: string,
+  ): Promise<CreateManyTransactionsDTO[]> {
+    const csv = await new Promise<Array<string[]>>(resolve => {
+      const listLineCsv: Array<string[]> = [];
+      fs.createReadStream(csvPath)
+        .pipe(csvparse())
+        .on('data', row => {
+          listLineCsv.push(row);
+        })
+        .on('end', () => {
+          resolve(listLineCsv);
+        });
+    });
+
+    const result: CreateManyTransactionsDTO[] = [];
+
+    const headers: string[] = csv[0].map(t => t.trim());
+
+    for (let i = 1; i < csv.length; i++) {
+      const currentline = csv[i];
+
+      const transaction: CreateManyTransactionsDTO = Object.assign(
+        {},
+        ...headers.map((k, idx) => ({ [k]: currentline[idx].trim() })),
+      );
+
+      result.push(transaction);
+    }
+
+    return result;
   }
 }
 
